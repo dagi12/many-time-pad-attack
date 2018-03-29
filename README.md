d9e1206c61cb150eda19bd864745751003e253c0
dagi12 <dagi12@o2.pl>
2018-03-30 00:27:18 +0200

    mini example

--- README.md
+++ README.md
@@ -4,9 +4,28 @@ This code investigates the properties of the one time pad - specifically that it
 
 Given 10 ciphertexts encrypted using the same key, we can break the encryption, and generate all the plaintexts
 
+XOR pairwise:
+1. \\x00 -> both ciphers was the same
+2. Space -> case flipped character
+3. Uppercase letter:
+   - space xor lowercase letter (same)
+   - punctuation xor lowercase letter
+4. Lowercase letter:
+   - space xor uppercase letter
+4. Numeric, Punctutation
+   - different letters, different case
+5. Control chars
+   - different letters, same case
+
 ~~Rewrite and understand~~
-2. Mini example
+~~2. Mini example~~
+<!-- str_xor(' sda','1234') =  '\x11AWU'
+str_xor('BS ,','1234') =  'saR\x18'
+str_xor('\x11AWU', 'saR\x18') = 'b \x05M' -->
 3. Rady z goo.gl/HvjcXp
+ - Those two cases should be pretty easy to tell apart. Furthermore, in the first case, you can easily get the actual characters at that position in all the plaintexts just by flipping the case of all the letters you obtained by XORing the ciphertexts together.
+ - Fill in letters
+ - debug 4,5 -> thm, the
 4. Python 3
 5. Work on bytes
 6. Language booster
--- my-attack.py
+++ my-attack.py
@@ -5,13 +5,34 @@ import collections
 import string
 
 PRINT_SET = string.printable
+KEY_LENGTH = 150
+SPACE_STRING = ' ' * KEY_LENGTH
 
-TARGET_CIPHER_DECODED = '1ba332ddb0b5fd07b5e4a8a472175565502ea1bf34490857d6e7c7b70b924358040a48582444a892bb244326d7a5314e3d883f460aabe942472dd3df39f3f368353782b0e17f59724be0ddac8143fc08ae2bba7cd43226212af57a25a56288ca6fed2133a1fd54e8261c171823c6efd0db8f7f40f65b8fbc37e5858def0cecc06ea18dc14746afac237bd9d72b49aa37121163311b6dfeaf245ce2013b9ebcfeab24dd25b049468c051bb6ca4b519050feab295932212f5b63613182bcf510ea29e757227294dec1a3e44584847b58e6eeedda2dbcb48d695259d36e736f4a6c31671ba81f3ac770c19f63f636690a2996a634bbe164c5e26efdecdfe11b1a23daac278eb5b4ee84b9e0ace36956437b587954b97db01150cef01cfa0b958758181592416a53a099ef657e7fdfbafc582558384604bfff0f5ebfcfd16bcbf07c3b23997ae03e5ebe08ea9435c489631aae226062d9e33c352aeb692cb47d8a846bf9723eac6c56ab3e42580d30dbefde9a81a903e51e99bd28ffc49ba00d22996fa0c8dd0dd5a4a62f28d3da2207bf781901933c08a7f2e6bb52ad183dd0a6b8df4cb10f8a41498f1384b1cc5814c411cdb3f85539372f4f757a6d9da5e51ea66eb0027528dfc6caf6b710c9c16d55ecadfdd673f9fcd9'.decode('hex')
+# TARGET_CIPHER_DECODED = '1ba332ddb0b5fd07b5e4a8a472175565502ea1bf34490857d6e7c7b70b924358040a48582444a892bb244326d7a5314e3d883f460aabe942472dd3df39f3f368353782b0e17f59724be0ddac8143fc08ae2bba7cd43226212af57a25a56288ca6fed2133a1fd54e8261c171823c6efd0db8f7f40f65b8fbc37e5858def0cecc06ea18dc14746afac237bd9d72b49aa37121163311b6dfeaf245ce2013b9ebcfeab24dd25b049468c051bb6ca4b519050feab295932212f5b63613182bcf510ea29e757227294dec1a3e44584847b58e6eeedda2dbcb48d695259d36e736f4a6c31671ba81f3ac770c19f63f636690a2996a634bbe164c5e26efdecdfe11b1a23daac278eb5b4ee84b9e0ace36956437b587954b97db01150cef01cfa0b958758181592416a53a099ef657e7fdfbafc582558384604bfff0f5ebfcfd16bcbf07c3b23997ae03e5ebe08ea9435c489631aae226062d9e33c352aeb692cb47d8a846bf9723eac6c56ab3e42580d30dbefde9a81a903e51e99bd28ffc49ba00d22996fa0c8dd0dd5a4a62f28d3da2207bf781901933c08a7f2e6bb52ad183dd0a6b8df4cb10f8a41498f1384b1cc5814c411cdb3f85539372f4f757a6d9da5e51ea66eb0027528dfc6caf6b710c9c16d55ecadfdd673f9fcd9'.decode('hex')
+target_cipher = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
+known_key_positions = set()
 
-SPACE_STRING = ' ' * 512
+def example_ciphers():
+    c1 = "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba50"
+    c2 = "234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb741"
+    c3 = "32510ba9a7b2bba9b8005d43a304b5714cc0bb0c8a34884dd91304b8ad40b62b07df44ba6e9d8a2368e51d04e0e7b207b70b9b8261112bacb6c866a232dfe257527dc29398f5f3251a0d47e503c66e935de812"
+    c4 = "32510ba9aab2a8a4fd06414fb517b5605cc0aa0dc91a8908c2064ba8ad5ea06a029056f47a8ad3306ef5021eafe1ac01a81197847a5c68a1b78769a37bc8f4575432c198ccb4ef63590256e305cd3a9544ee41"
+    c5 = "3f561ba9adb4b6ebec54424ba317b564418fac0dd35f8c08d31a1fe9e24fe56808c213f17c81d9607cee021dafe1e001b21ade877a5e68bea88d61b93ac5ee0d562e8e9582f5ef375f0a4ae20ed86e935de812"
+    c6 = "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd2061bbde24eb76a19d84aba34d8de287be84d07e7e9a30ee714979c7e1123a8bd9822a33ecaf512472e8e8f8db3f9635c1949e640c621854eba0d"
+    c7 = "32510bfbacfbb9befd54415da243e1695ecabd58c519cd4bd90f1fa6ea5ba47b01c909ba7696cf606ef40c04afe1ac0aa8148dd066592ded9f8774b529c7ea125d298e8883f5e9305f4b44f915cb2bd05af513"
+    c8 = "315c4eeaa8b5f8bffd11155ea506b56041c6a00c8a08854dd21a4bbde54ce56801d943ba708b8a3574f40c00fff9e00fa1439fd0654327a3bfc860b92f89ee04132ecb9298f5fd2d5e4b45e40ecc3b9d59e941"
+    c9 = "271946f9bbb2aeadec111841a81abc300ecaa01bd8069d5cc91005e9fe4aad6e04d513e96d99de2569bc5e50eeeca709b50a8a987f4264edb6896fb537d0a716132ddc938fb0f836480e06ed0fcd6e9759f404"
+    c10 = "466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff503"
+    return [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10]
+
+def exercise_ciphers():
+    ciphers = []
+    for i in range(0, 20):
+        ciphers.append(read_cipher_file(i))
+        return ciphers
 
 
-def str_xor(a, b):  # xor two strings (trims the longer input)
+def str_xor(a, b):
     return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b)])
 
 
@@ -21,10 +42,8 @@ def read_cipher_file(i):
 
 
 def main():
-    ciphers = []
-    for i in range(0, 20):
-        ciphers.append(read_cipher_file(i))
-    final_key = [None] * 512
+    ciphers = example_ciphers()
+    final_key = [None] * KEY_LENGTH
     for i1, txt1 in enumerate(ciphers):
 
         counter = collections.Counter()
@@ -40,14 +59,16 @@ def main():
 
         space_indexes = []
         for i, val in counter.items():
-            if val > 10:
+            if val > 5:
                 space_indexes.append(i)
         xor_with_spaces = str_xor(txt1_decoded, SPACE_STRING)
         for i in space_indexes:
             final_key[i] = xor_with_spaces[i].encode('hex')
+            known_key_positions.add(i)
 
     final_key_hex = ''.join(val if val is not None else '00' for val in final_key)
     final_key_hex_decoded = final_key_hex.decode('hex')
-    print str_xor(TARGET_CIPHER_DECODED, final_key_hex_decoded)
+    output = str_xor(target_cipher.decode('hex'), final_key_hex_decoded)
+    print ''.join([char if char in PRINT_SET and index in known_key_positions else '*' for index, char in enumerate(output)])
 
 main()
